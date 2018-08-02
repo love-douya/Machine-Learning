6f949970f6abbd02f84e0c8d7d0f5218538cf4bc
Jimmy Tao <jimmyt@wicresoft.com>
2018-08-02 09:32:08 +0800

    Update

--- Text Classification.py
+++ Text Classification.py
@@ -10,5 +10,24 @@ from keras import layers, models, optimizers
 
 # load the dataset
 
-data = open('../amazon_review_full_csv/train.csv').read()
-data = open()
+data = open('data/corpus').read()
+labels, texts = [], []
+for i, line in enumerate(data.split('\n')):
+    content = line.split()
+    labels.append(content[0])
+    texts.append(content[1])
+
+# create a dataframe using texts and labels
+trainDF = pandas.DataFrame()
+trainDF['text'] = texts
+trainDF['label'] = labels
+
+# Feature Engineering
+
+# Count Vectors as features
+count_vect = CountVectorizer(analyzer = 'word', token_pattern = r'\w{1,}')
+count_vect.fit(trainDF['text'])
+
+# transform the training and validation data using count vectorizer object
+xtrain_count = count_vect.transform(train_x)
+xvalid_count = count_vect.transform(valid_x)
