2c790816d48050a0a9aea21c44dd8f521f061a42
love-douya <936165716@qq.com>
2018-09-09 22:24:46 +0800

    Update

---
+++ Reduce_Demo.py
@@ -0,0 +1,28 @@
+import sys
+
+current_word = None
+current_count = 0
+word = None
+
+for line in sys.stdin:
+    line = line.strip()
+    word, count = line.split('\t', 1)
+
+    try:
+        count = int(count)
+    except ValueError:
+        continue
+
+    if current_word == word:
+        current_count += count
+    else:
+        if current_word:
+            print("%s\t%s", current_word, current_count)
+
+        current_count = count
+        current_word
+
+#输出最后一个word统计
+if current_word == word:
+    print("%s\t%s", current_word, current_count)
+    
